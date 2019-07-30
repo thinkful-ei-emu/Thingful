557047a30ffcd8feaa30a95ca368cc0903c790dd
Jvymvn <Jordon.carter@yahoo.com>
2019-07-30 15:46:57 -0400

    Updates

--- thingful-server/src/middlewear/basic-auth.js
+++ thingful-server/src/middlewear/basic-auth.js
@@ -1,4 +1,5 @@
 const AuthService = require('../auth/auth-service')
+const bcrypt = require('bcryptjs')
 
 function requireAuth(req, res, next) {
     console.log('Basic auth running')
@@ -23,11 +24,17 @@ function requireAuth(req, res, next) {
         tokenUserName
     )
         .then(user => {
-            if (!user || user.password !== tokenPassword) {
+            if (!user) {
                 return res.status(401).json({ error: 'Unauthorized request' })
             }
-            req.user = user
-            next()
+            return bcrypt.compare(tokenPassword, user.password)
+                .then(passwordsMatch => {
+                    if (!passwordsMatch) {
+                        return res.status(401).json({ error: 'Unauthorized request' })
+                    }
+                    req.user = user
+                    next()
+                })
         })
         .catch(next)
 }
